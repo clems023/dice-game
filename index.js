017f4ec1a00254a0019a95c30b4d54c67c4a9075
Clément Adouna <clementadouna@gmail.com>
2022-12-09 12:22:58 +0000

    commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+
+switch (randomNumber1) {
+    case 1:
+        document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
+        break;
+    case 2:
+        document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
+        break;
+    case 3:
+        document.querySelector(".img1").setAttribute("src", "./images/dice3.png");
+        break;
+    case 4:
+        document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
+        break;
+    case 5:
+        document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
+        break;
+    case 6:
+        document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
+        break;
+
+    default:
+        break;
+}
+
+switch (randomNumber2) {
+    case 1:
+        document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
+        break;
+    case 2:
+        document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
+        break;
+    case 3:
+        document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
+        break;
+    case 4:
+        document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
+        break;
+    case 5:
+        document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
+        break;
+    case 6:
+        document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
+        break;
+
+    default:
+        break;
+}
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container h1").innerHTML = "🚩 Player 1 wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector(".container h1").innerHTML = "Player 2 wins 🚩";
+} else {
+    document.querySelector(".container h1").innerHTML = "Draw !";
+}
